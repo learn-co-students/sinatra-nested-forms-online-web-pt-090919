057b41c617c0f73ae884474d034bb94062fae749
Srikalyan Megati <kalmegati@gmail.com>
2019-12-13 00:11:01 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,11 @@
 class Pirate
+  
+  attr_accessor :name, :weight, :height
+  
+  def initialize(attrs)
+    @name = attrs[:name]
+    @weight = attrs[:weight]
+    @height = attrs[:height]
+  end
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty
+  
+  @@ships = []
+  
+  def initialize(attrs)
+    @name = attrs[:name]
+    @type = attrs[:type]
+    @booty = attrs[:booty]
+    @@ships << self
+  end
+  
+  def self.all
+    @@ships
+  end
+  
+  def self.clear
+    @@ships = []
+  end
+  
 end
